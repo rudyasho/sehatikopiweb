60d553123994640a715d4f6c7a3ffd3d7915d79e
Roedy Rustam <rd.lapawawoi@gmail.com>
2025-08-31 08:06:44 +0000

    setela mode tampilan  light secara default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
       <body className={cn('min-h-screen bg-background font-body antialiased', poppins.variable, playfairDisplay.variable, robotoMono.variable)}>
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
         >
