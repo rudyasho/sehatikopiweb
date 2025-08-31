9efa30dbbca9f11d960b33eea4212e54d5ee33ba
Roedy Rustam <rd.lapawawoi@gmail.com>
2025-08-31 18:21:43 +0000

    izinkan domain pengembangan ini di dalam file konfigurasi Next.js

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https-alias',
+        hostname: 'placehold.co'
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
