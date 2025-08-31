413adba72e1f1dce24c0c759583793c5cd5595ba
Roedy Rustam <rd.lapawawoi@gmail.com>
2025-08-31 15:41:51 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBWJoWerUztk9opWD1J6I45TwhdoDp6

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 // Client-side Firebase configuration should ONLY use public environment variables.
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBWJoWerUztk9opWD1J6I45TwhdoDp6dnY",
+  authDomain: "sehati-kopi-digital-fy12l.firebaseapp.com",
+  projectId: "sehati-kopi-digital-fy12l",
+  storageBucket: "sehati-kopi-digital-fy12l.appspot.com",
+  messagingSenderId: "1013267327927",
+  appId: "1:1013267327927:web:fce6c5d0f7e2b834871632"
 };
 
 function initializeFirebaseApp(config: FirebaseOptions) {
